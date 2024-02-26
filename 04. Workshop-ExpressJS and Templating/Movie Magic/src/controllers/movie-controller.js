e77a424d1bae39742bd49f1c69e2c29775d34166
lsfckt <132505560+lsfckt@users.noreply.github.com>
2024-02-26 22:17:18 +0200

    render details page

--- 04. Workshop-ExpressJS and Templating/Movie Magic/src/controllers/movie-controller.js
+++ 04. Workshop-ExpressJS and Templating/Movie Magic/src/controllers/movie-controller.js
@@ -15,4 +15,8 @@ router.post('/create', (req, res) => {
     res.redirect('/');
 });
 
+router.get('/details', (req, res) => {
+    res.render('details');
+});
+
 module.exports = router;
--- 04. Workshop-ExpressJS and Templating/Movie Magic/src/services/movie-service.js
+++ 04. Workshop-ExpressJS and Templating/Movie Magic/src/services/movie-service.js
@@ -1,6 +1,8 @@
 const movies = [];
 
 exports.create = (movieData) => {
+
+    movieData._id = movies[movies.length - 1]._id + 1;
     movies.push(movieData);
 }
 
