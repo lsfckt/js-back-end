ea9e501aa7fba783ec8f994e3cf27a2a29a8cad2
lsfckt <132505560+lsfckt@users.noreply.github.com>
2024-02-27 21:08:53 +0200

    search functionality

--- 04. Workshop-ExpressJS and Templating/Movie Magic/src/controllers/home-controller.js
+++ 04. Workshop-ExpressJS and Templating/Movie Magic/src/controllers/home-controller.js
@@ -12,12 +12,13 @@ router.get('/', (req, res) => {
 
 router.get('/about', (req, res) => {
     res.render('about');
-})
+});
 
 router.get('/search', (req, res) => {
-    let movies = movieService.getAll();
+    const { title, genre, year } = req.query;
+    let movieResult = movieService.search(title, genre, year);
 
-    res.render('search', { movies });
+    res.render('search', { movies: movieResult, title, genre, year });
 });
 
 router.get('/404', (req, res) => {
--- 04. Workshop-ExpressJS and Templating/Movie Magic/src/services/movie-service.js
+++ 04. Workshop-ExpressJS and Templating/Movie Magic/src/services/movie-service.js
@@ -21,3 +21,20 @@ exports.getOne = (movieId) => {
     return movie;
 }
 
+exports.search = (title, genre, year) => {
+    let result = movies.slice();
+    
+    if (title) {
+        result = result.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase()));
+    }
+
+    if (genre) {
+        result = result.filter(movie => movie.genre.toLowerCase() === genre.toLowerCase());
+    }
+
+    if (year) {
+        result = result.filter(movie => movie.year === year);
+    }
+
+    return result;
+}
