1c72d69a0272fb2e11d6891e8f5001683e25fd4a
lsfckt <132505560+lsfckt@users.noreply.github.com>
2024-02-27 20:26:51 +0200

    show dynamic movies on search page

--- 04. Workshop-ExpressJS and Templating/Movie Magic/src/controllers/home-controller.js
+++ 04. Workshop-ExpressJS and Templating/Movie Magic/src/controllers/home-controller.js
@@ -14,6 +14,12 @@ router.get('/about', (req, res) => {
     res.render('about');
 })
 
+router.get('/search', (req, res) => {
+    let movies = movieService.getAll();
+
+    res.render('search', { movies });
+});
+
 router.get('/404', (req, res) => {
     res.render('404');
 });
