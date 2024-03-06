bad709135af8ab9f92365280581598587cbac223
lsfckt <132505560+lsfckt@users.noreply.github.com>
2024-03-06 22:36:02 +0200

    add new resources

--- 04. Workshop-ExpressJS and Templating/Movie Magic/src/config/express-config.js
+++ 04. Workshop-ExpressJS and Templating/Movie Magic/src/config/express-config.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 function expressConfig(app) {
-    app.use(express.static(path.resolve('./src/public')));
+    app.use(express.static(path.resolve('./src/static')));
     app.use(express.urlencoded({ extended: false }));
 }
 
