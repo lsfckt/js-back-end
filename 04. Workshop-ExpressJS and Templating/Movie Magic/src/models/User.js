508cbc29d30cf3d0ebb0c2d5511c425377d1294f
lsfckt <132505560+lsfckt@users.noreply.github.com>
2024-03-10 12:38:10 +0200

    create User model

---
+++ 04. Workshop-ExpressJS and Templating/Movie Magic/src/models/User.js
@@ -0,0 +1,28 @@
+const { Schema, mongoose } = require('mongoose');
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: [true, 'Email is required!'],
+        lowercase: true,
+        match: /\w+@\w+.\w+/i,
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required!'],
+        minLength: 4,
+        validate: {
+            validator: function (v) {
+                return /^ (?=.* [A - Za - z])(?=.*\d)[A - Za - z\d]{ 4, }$/i.test(v);
+            }
+        },
+        message: props => `${props.value} is not valid password, it must have minimum four characters, at least one letter and one number`;
+    },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exporst = User;
+
