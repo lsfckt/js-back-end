9fe3af0401208ed9c562ed4bc32f1087166807d2
lsfckt <132505560+lsfckt@users.noreply.github.com>
2024-03-10 13:17:40 +0200

    send user data to db

--- 04. Workshop-ExpressJS and Templating/Movie Magic/src/controllers/auth-controller.js
+++ 04. Workshop-ExpressJS and Templating/Movie Magic/src/controllers/auth-controller.js
@@ -1,9 +1,23 @@
 const router = require('express').Router();
 
-
+const authService = require('../services/auth-service');
 
 router.get('/register', (req, res) => {
     res.render('auth/register');
+});
+
+router.post('/register', async (req, res) => {
+    const userData = req.body;
+
+    try {
+        await authService.register(userData);
+    } catch (error) {
+        console.log(error.message);
+        return res.redirect('/register');
+    }
+
+    res.redirect('/login');
+
 });
 
 module.exports = router;
--- 04. Workshop-ExpressJS and Templating/Movie Magic/src/models/User.js
+++ 04. Workshop-ExpressJS and Templating/Movie Magic/src/models/User.js
@@ -3,19 +3,19 @@ const { Schema, mongoose } = require('mongoose');
 const userSchema = new Schema({
     email: {
         type: String,
-        required: [true, 'Email is required!'],
+        required: true,
         lowercase: true,
-        match: /\w+@\w+.\w+/i,
+        match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i,
         trim: true,
         unique: true,
     },
     password: {
         type: String,
-        required: [true, 'Password is required!'],
+        required: true,
         minLength: 4,
         validate: {
-            validator: function (v) {
-                return /^ (?=.* [A - Za - z])(?=.*\d)[A - Za - z\d]{ 4, }$/i.test(v);
+            validator: function(v) {
+                return /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,}$/.test(v);
             }
         },
         message: props => `${props.value} is not valid password, it must have minimum four characters, at least one letter and one number`,
@@ -24,5 +24,5 @@ const userSchema = new Schema({
 
 const User = mongoose.model('User', userSchema);
 
-module.exporst = User;
+module.exports = User;
 
