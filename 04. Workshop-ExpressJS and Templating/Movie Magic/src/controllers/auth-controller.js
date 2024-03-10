9ae1c1df3eb8e42be740202ba75528c6d441546c
lsfckt <132505560+lsfckt@users.noreply.github.com>
2024-03-10 13:29:02 +0200

    render login page

--- 04. Workshop-ExpressJS and Templating/Movie Magic/src/controllers/auth-controller.js
+++ 04. Workshop-ExpressJS and Templating/Movie Magic/src/controllers/auth-controller.js
@@ -20,4 +20,8 @@ router.post('/register', async (req, res) => {
 
 });
 
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
 module.exports = router;
--- 04. Workshop-ExpressJS and Templating/Movie Magic/src/models/User.js
+++ 04. Workshop-ExpressJS and Templating/Movie Magic/src/models/User.js
@@ -3,7 +3,7 @@ const { Schema, mongoose } = require('mongoose');
 const userSchema = new Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, 'Email is required!'],
         lowercase: true,
         match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i,
         trim: true,
@@ -11,14 +11,14 @@ const userSchema = new Schema({
     },
     password: {
         type: String,
-        required: true,
+        required: [true, 'Password is required!'],
         minLength: 4,
         validate: {
-            validator: function(v) {
+            validator: function (v) {
                 return /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,}$/.test(v);
-            }
+            },
+            message: props => `${props.value} is not valid password, it must have minimum four characters, at least one letter and one number`,
         },
-        message: props => `${props.value} is not valid password, it must have minimum four characters, at least one letter and one number`,
     },
 });
 
